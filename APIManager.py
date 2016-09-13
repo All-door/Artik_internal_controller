66c486f9388ca1be87d7da044070d3e1051ac837
Inho Choi <inhochoi@Inhoui-MacBook-Pro.local>
2016-09-13 16:53:41 +0900

    APIManager: 초기 파일 생성

---
+++ APIManager.py
@@ -0,0 +1,15 @@
+import json
+import http.client
+
+class APIManager(object):
+    """All-Door for Artik API 통신관련 라이브러리"""
+    def __init__(self, apiUrl, redisUrl, time):
+        super(APIManager, self).__init__()
+        self.arg = arg
+    def start(self):
+        pass
+    def stop(self):
+        pass
+
+if __name__ == '__main__':
+    print('API Manager start')
