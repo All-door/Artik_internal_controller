ede0586ad8f052b8cb6956f0ea3c0688274754cc
Inho Choi <inhochoi@Inhoui-MacBook-Pro.local>
2016-09-13 17:21:29 +0900

    APIManager: 일정 주기마다 통신후 redis에 임시비밀번호 저장 기능 구현

--- APIManager.py
+++ APIManager.py
@@ -1,15 +1,55 @@
 import json
 import http.client
+import redis
+import time
 
 class APIManager(object):
     """All-Door for Artik API 통신관련 라이브러리"""
-    def __init__(self, apiUrl, redisUrl, time):
+    def __init__(self, apiUrl='localhost:3000', redisUrl='localhost', redisPort=6379, deviceId='1',tickSecond=5):
+        print('API Manager Init...')
         super(APIManager, self).__init__()
-        self.arg = arg
-    def start(self):
-        pass
-    def stop(self):
-        pass
+        self.apiUrl = apiUrl
+        self.redisUrl = redisUrl
+        self.redisPort = redisPort
+        self.deviceId = deviceId
+        self.tickSecond = tickSecond
+        self.stop = False
+        self.payload = "{\"battery_status\" : 0}"
+        self.headers = {
+            'content-type': "application/json",
+            'cache-control': "no-cache",
+            }
+        self.printArgument()
+        self.initRedis()
+
+    def printArgument(self):
+        print('API URL :', self.apiUrl)
+        print('Redis URL :', self.redisUrl)
+        print('Reids PORT:', self.redisPort)
+        print('Device ID :', self.deviceId)
+        print('Tick Second :', self.tickSecond,'s')
+
+    def initRedis(self):
+        self.redis = redis.StrictRedis(host=self.redisUrl, port=self.redisPort, db=0)
+
+    def routine(self):
+        while True:
+            try:
+                conn = http.client.HTTPConnection(self.apiUrl)
+                conn.request("POST", "/api/device/" + self.deviceId, self.payload, self.headers)
+                data = conn.getresponse().read()
+                data_json = json.loads(data.decode("utf-8"))
+
+                print('Password :', data_json['pw1'])
+                print('FaceId :', None)
+                print('Artik Cloud Device Id :', None)
+
+                self.redis.set('Password', data_json["pw1"])
+                self.redis.set('FaceId', None)
+                self.redis.set('ArtikDeviceId', None)
+            except Exception as e:
+                print(e)
+            time.sleep(self.tickSecond)
 
 if __name__ == '__main__':
-    print('API Manager start')
+    APIManager().routine()
