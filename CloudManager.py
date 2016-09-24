1d9b8fceba4ebe9d8eb601c9e1f66d3ebe978343
inhochoi <revaer@naver.com>
2016-09-24 21:39:29 +0900

    CloudManager: 기본 기능 구현 완료, 센서값 데이터는 랜덤값으로 설정

--- CloudManager.py
+++ CloudManager.py
@@ -1,24 +1,43 @@
+from random import randrange, choice
 import json
+import http.client
 import time
 import redis
 
 class CloudManager(object):
-    ARTIK_CLOUD_URL = '';
-    def __init__(self, deviceId, redisUrl='localhost', redisPort=6379, tickSecond=30):
+    ARTIK_CLOUD_MESSAGE_URL = 'api.artik.cloud'
+    def __init__(self, deviceId, accessToken, tickSecond=10):
         super(CloudManager, self).__init__()
         self.deviceId = deviceId
-        self.redisUrl = redisUrl
-        self.redisPort = redisPort
+        self.accessToken = accessToken
         self.tickSecond = tickSecond
-
     def routine(self):
+        headers = {
+            'authorization': "Bearer " + self.accessToken,
+            'content-type': "application/json",
+            }
+        payload = dict()
+        payload['sdid'] = self.deviceId
+        payload['type'] = 'message'
+
         while True:
             try:
-                print('Rotuine')
+                conn = http.client.HTTPSConnection(self.ARTIK_CLOUD_MESSAGE_URL)
+                payload['data'] = '{"Temperature" : '+ str(randrange(1,30)) + ',"Humidity" : ' + str(randrange(0,100)) + ',"Hall" : ' + choice(['true','false']) + '}'
+
+                conn.request("POST", "/v1.1/messages", json.dumps(payload), headers)
+                res = conn.getresponse()
+                data = res.read()
             except Exception as e:
                 print(e)
             time.sleep(self.tickSecond)
 
 
 if __name__ == '__main__':
-    CloudManager("").routine();
+    redisClient = redis.StrictRedis(host='localhost', port=6379, db=0)
+    deviceId = redisClient.get('ArtikDeviceId').decode('utf-8')
+    accessToken = redisClient.get('ArtikDeviceAccessToken').decode('utf-8')
+    CloudManager(
+        deviceId = deviceId,
+        accessToken = accessToken
+    ).routine();
