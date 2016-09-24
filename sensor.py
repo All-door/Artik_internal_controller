d93cdda5ae19bf4830a88f5ee76b4f248c16d690
NayongLee <iny003@naver.com>
2016-09-24 23:32:20 +0900

    중복된 부분이 생기는 온습도 함수를 이름을 getTemperHumi로 변경, 온도, 습도 순차 출력으로 변경했습니다.

--- sensor.py
+++ sensor.py
@@ -37,7 +37,7 @@ class Sensor:
 	    print ("Pin " , str(self.pinNum), " has been exported")
 	pinctl.close()
 	
-    def getTemperature(self):
+    def getTemperHumi(self):
         setup()
         set_input()
 
@@ -122,95 +122,8 @@ class Sensor:
 	Temperature = bin2dec(TemperatureBit)
 		
 	if int(Humidity) + int(Temperature) - int(bin2dec(crc)) == 0:
-	    return Temperature
+	    return Temperature, Humidity
 	else:
 	    return "ERR_CRC"
 			
-    def getHumidity(self):
-	setup()
-        set_input()
-
-        filename = '/sys/class/gpio/gpio%d/value' % self.pinNum
-        pin = open(filename, "wb", 0)
-
-        pin.write(str(1))
-        time.sleep(0.025)
-
-	pin.write(str(0))
-	time.sleep(0.02)
-
-	pin.close()
-	exit_gpio()
-
-	for i in range(0,500):
-    	    pin = open(filename, "rb", 0)
-	    data.append(pin.read())
-            pin.close()
-
-        exit_gpio()
-	
-        bit_count = 0
-        tmp = 0
-        count = 0
-        HumidityBit = ""
-        TemperatureBit = ""
-        crc = ""
-
-        try:
-            while data[count] == 1:
-	        tmp = 1
-		count = count + 1
-
-	    for i in range(0, 32):
-		bit_count = 0
-
-		while data[count] == 0:
-		    tmp = 1
-		    count = count + 1
-
-		while data[count] == 1:
-		    bit_count = bit_count + 1
-		    count = count + 1
-
-		if bit_count > 3:
-		    if i>=0 and i<8:
-			HumidityBit = HumidityBit + "1"
-		    if i>=16 and i<24:
-			TemperatureBit = TemperatureBit + "1"
-		else:
-		    if i>=0 and i<8:
-			HumidityBit = HumidityBit + "0"
-		    if i>=16 and i<24:
-			TemperatureBit = TemperatureBit + "0"
-
-	except:
-	    print ("ERR_RANGE")
-	    exit(0)
-
-	try:
-	    for i in range(0, 8):
-		bit_count = 0
-	
-		while data[count] == 0:
-		    tmp = 1
-		    count = count + 1
-
-		while data[count] == 1:
-		    bit_count = bit_count + 1
-		    count = count + 1
-
-		if bit_count > 3:
-		    crc = crc + "1"
-		else:
-		    crc = crc + "0"
-	except:
-	    print ("ERR_RANGE")
-	    exit(0)
-
-	Humidity = bin2dec(HumidityBit)
-	Temperature = bin2dec(TemperatureBit)
-
-	if int(Humidity) + int(Temperature) - int(bin2dec(crc)) == 0:
-	    return Humidity
-	else:
-	    return "ERR_CRC"
+ 
