88a0f17643ecb0aa765552dd235bda5af4710f00
NayongLee <iny003@naver.com>
2016-10-02 21:44:41 +0900

    cds sensor method제거 및 변경

--- cdsSensor.py
+++ cdsSensor.py
@@ -1,26 +1,19 @@
 import sys
-import time
 
 class CdsSensor:
 
     def __init__(self, pinNum):
         self.pinNum = pinNum
 
-    def setup(self):
-        filename = '/sys/devices/12d10000.adc/iio:device0/in_voltage%d_raw' % self.pinNum
-        pinctldir = open(filename, "rb", 0)
-        pinctldir.close()
-
-    def RCtime(self):
-        self.setup()
+    def getBright(self):
         filename = '/sys/devices/12d10000.adc/iio:device0/in_voltage%d_raw' % self.pinNum
         pin = open(filename, "rb", 0)
         readPin = pin.read()
         voltage = int(readPin)*(5/4095)
         lux = (voltage-0.022)*100/(0.666)
-
+        pin.close()
         return round(lux,4)
 
 if __name__=="__main__":
     sensor = CdsSensor(0)
-    print("Lux :",sensor.RCtime())
+    print("Lux :",sensor.getBright())
