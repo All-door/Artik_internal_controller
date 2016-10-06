718af3661fb8fa6871169fc717ee2e0d83f3a1e0
inhochoi <revaer@naver.com>
2016-10-06 21:42:13 +0900

    CloudManager: 주기마다 Redis에서 deviceID, accessToken 받아오는 것으로 변경

--- CloudManager.py
+++ CloudManager.py
@@ -6,38 +6,38 @@ import redis
 
 class CloudManager(object):
     ARTIK_CLOUD_MESSAGE_URL = 'api.artik.cloud'
-    def __init__(self, deviceId, accessToken, tickSecond=60 * 10):
+    def __init__(self, tickSecond=60 * 10):
         super(CloudManager, self).__init__()
-        self.deviceId = deviceId
-        self.accessToken = accessToken
         self.tickSecond = tickSecond
-    def routine(self):
-        headers = {
-            'authorization': "Bearer " + self.accessToken,
-            'content-type': "application/json",
-            }
-        payload = dict()
-        payload['sdid'] = self.deviceId
-        payload['type'] = 'message'
+        self.redisClient = redis.StrictRedis(host='localhost', port=6379, db=0)
 
+    def routine(self):
         while True:
             try:
-                conn = http.client.HTTPSConnection(self.ARTIK_CLOUD_MESSAGE_URL)
-                payload['data'] = '{"CO2" : '+ str(randrange(1,30)) + ',"SMOKE" : ' + str(randrange(0,100))+ ',"LPG" : ' + str(randrange(0,100)) + ',"Hall" : ' + choice(['true','false']) + '}'
+                deviceId = self.redisClient.get('ArtikDeviceId').decode('utf-8')
+                accessToken = self.redisClient.get('ArtikDeviceAccessToekn').decode('utf-8')
 
-                conn.request("POST", "/v1.1/messages", json.dumps(payload), headers)
-                res = conn.getresponse()
-                data = res.read()
+                if deviceId == 'None' or accessToken == 'None':
+                    print('DevcieID, AccessToken is None')
+                else:
+                    headers = {
+                        'authorization': "Bearer " + accessToken,
+                        'content-type': "application/json",
+                        }
+                    payload = dict()
+                    payload['sdid'] = deviceId
+                    payload['type'] = 'message'
+
+                    conn = http.client.HTTPSConnection(self.ARTIK_CLOUD_MESSAGE_URL)
+                    payload['data'] = '{"CO2" : '+ str(randrange(1,30)) + ',"SMOKE" : ' + str(randrange(0,100))+ ',"LPG" : ' + str(randrange(0,100)) + ',"Hall" : ' + choice(['true','false']) + '}'
+
+                    conn.request("POST", "/v1.1/messages", json.dumps(payload), headers)
+                    res = conn.getresponse()
+                    data = res.read()
             except Exception as e:
                 print(e)
             time.sleep(self.tickSecond)
 
 
 if __name__ == '__main__':
-    redisClient = redis.StrictRedis(host='localhost', port=6379, db=0)
-    deviceId = redisClient.get('ArtikDeviceId').decode('utf-8')
-    accessToken = redisClient.get('ArtikDeviceAccessToekn').decode('utf-8')
-    CloudManager(
-        deviceId = deviceId,
-        accessToken = accessToken
-    ).routine();
+    CloudManager().routine();
